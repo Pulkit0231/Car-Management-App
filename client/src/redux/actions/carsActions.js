e2cb46feebc90c1856d9e775327cb6376cb2c8b5
Pulkit <plktnagar@gamil.com>
2024-11-15 15:15:03 +0530

    added live server link

--- client/src/redux/actions/carsActions.js
+++ client/src/redux/actions/carsActions.js
@@ -6,7 +6,7 @@ export const getAllCars = () => async dispatch => {
 
     dispatch({ type: 'LOADING', payload: true })
     try {
-        const response = await axios.get('http://localhost:5000/api/cars/getallcars')
+        const response = await axios.get('https://car-management-app-teal.vercel.app/api/cars/getallcars')
         dispatch({ type: 'GET_ALL_CARS', payload: response.data })
         dispatch({ type: 'LOADING', payload: false })
 
@@ -23,7 +23,7 @@ export const addCar = (reqObj) => async dispatch => {
 
     dispatch({ type: 'LOADING', payload: true })
     try {
-        await axios.post('http://localhost:5000/api/cars/addcar', reqObj)
+        await axios.post('https://car-management-app-teal.vercel.app/api/cars/addcar', reqObj)
 
         dispatch({ type: 'LOADING', payload: false })
         message.success('New car added successfully')
@@ -42,7 +42,7 @@ export const editCar = (reqObj) => async dispatch => {
 
     dispatch({ type: 'LOADING', payload: true })
     try {
-        await axios.post('http://localhost:5000/api/cars/editcar', reqObj)
+        await axios.post('https://car-management-app-teal.vercel.app/api/cars/editcar', reqObj)
 
         dispatch({ type: 'LOADING', payload: false })
         message.success('Car details updated successfully')
@@ -61,7 +61,7 @@ export const deleteCar = (reqObj) => async dispatch => {
 
     dispatch({ type: 'LOADING', payload: true })
     try {
-        await axios.post('http://localhost:5000/api/cars/deletecar', reqObj)
+        await axios.post('https://car-management-app-teal.vercel.app/api/cars/deletecar', reqObj)
 
         dispatch({ type: 'LOADING', payload: false })
         message.success('Car deleted successfully')
